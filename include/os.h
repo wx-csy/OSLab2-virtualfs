e6a5ba72bac8f4bb3bd187bdfe1454e9f920d05a
tracer <tracer@tracer.org>
2018-04-26 20:20:05 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- include/os.h
+++ include/os.h
@@ -3,10 +3,13 @@
 
 #include <kernel.h>
 
+/* This function has been implemented in libc. So we no longer
+ * need it here.
 static inline void puts(const char *p) {
   for (; *p; p++) {
     _putc(*p);
   }
 }
+*/
 
 #endif
