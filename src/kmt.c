7165406314a578b12b03ef15a719e19e29995127
tracer <tracer@tracer.org>
2018-04-26 21:31:44 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -5,12 +5,12 @@ static int kmt_create(thread_t *thread, void (*entry)(void *arg),
     void *arg);
 static void kmt_teardown(thread_t *thread);
 thread_t *kmt_schedule();
-void kmt_spin_init(spinlock_t *lk, const char *name);
-void kmt_spin_lock(spinlock_t *lk);
-void kmt_spin_unlock(spinlock_t *lk);
-void kmt_sem_init(sem_t *sem, const char *name, int value);
-void kmt_sem_wait(sem_t *sem);
-void kmt_sem_signal(sem_t *sem);
+static void kmt_spin_init(spinlock_t *lk, const char *name);
+static void kmt_spin_lock(spinlock_t *lk);
+static void kmt_spin_unlock(spinlock_t *lk);
+static void kmt_sem_init(sem_t *sem, const char *name, int value);
+static void kmt_sem_wait(sem_t *sem);
+static void kmt_sem_signal(sem_t *sem);
 
 MOD_DEF(kmt) {
   .init = kmt_init,
