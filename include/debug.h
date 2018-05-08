b60a2a3ca4cac0e61a985275f2f70f043e3db7db
tracer <tracer@tracer.org>
2018-05-08 15:21:01 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- include/debug.h
+++ include/debug.h
@@ -2,7 +2,7 @@
 #define __DEBUG_H__
 
 #ifdef DEBUG_ME
-# define $debug(fmt, ...) printf("\033[94m%s: " fmt "%\n\033[0m", \
+# define $debug(fmt, ...) printf("\033[94m%s: " fmt "\n\033[0m", \
     __func__, ##__VA_ARGS__) 
 #else 
 # define $debug(...) ((void) 0)
