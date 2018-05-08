7d4a0e15a4f32fd1a97a29ee214d72f3c91111e4
tracer <tracer@tracer.org>
2018-05-08 15:44:44 +0800

    > clean sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- include/debug.h
+++ include/debug.h
@@ -2,10 +2,10 @@
 #define __DEBUG_H__
 
 #ifdef DEBUG_ME
-# define $debug(fmt, ...) printf("\033[94m%s: " fmt "\n\033[0m", \
+# define _debug(fmt, ...) printf("\033[94m%s: " fmt "\n\033[0m", \
     __func__, ##__VA_ARGS__) 
 #else 
-# define $debug(...) ((void) 0)
+# define _debug(...) ((void) 0)
 #endif
 
 #endif
