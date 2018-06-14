f8094756bbee1c8f2722156ff0873f046cd6e7e9
tracer <tracer@tracer.org>
2018-06-14 20:58:47 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/test/devfs_test.c
+++ src/test/devfs_test.c
@@ -14,15 +14,43 @@ static void term_getline(char *buf) {
   buf[sz - 1] = 0;
 }
 
+static void cmd_exit() {
+  _Exit(0);
+}
+
+static void cmd_fd() {
+  for (int i = 0; i < 16; i++) {
+    printf("fd[%d] = %p\n", i, this_thread->fd[i]);
+  }
+}
+
+struct cmd {
+  const char *cmd;
+  void (*func)();
+} cmds[] = {
+  {"exit", cmd_exit},
+  {"fd", cmd_fd}
+};
+
+#define NR_CMD  (sizeof(cmds) / sizeof(struct cmd))
+
 void devfs_test(void *igonre) {
   fd = vfs->open("/dev/stdin", O_RDONLY);
   int data = 0;
   printf("Welcome to OSLab2!\n");
   while (1) {
-    printf("> ");
+    printf("$ ");
     char buf[256];
     term_getline(buf);
     printf("%s\n", buf);
+    for (int i = 0; i < NR_CMD; i++) {
+      if (strcmp(buf, cmds[i].cmd) == 0) {
+        cmds[i].func();
+        goto next;
+      }
+    } 
+    printf("Command `%s' not found!", buf);
+next:;
   }
   printf("random: %d\n", data);
 }
