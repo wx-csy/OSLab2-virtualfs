30c2fdea8a9a5e08dc053c5ac33b977975bde869
tracer <tracer@tracer.org>
2018-06-16 19:01:13 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/test/test.c
+++ src/test/test.c
@@ -60,7 +60,7 @@ void shb_type(const char *path) {
   buf = pmm->alloc(length);
   vfs->read(fd, buf, length);
   printf("%s", buf);
-  puts("~")
+  puts("\033[97m~\033[0m");
   pmm->free(buf);
 
   RETURN;
