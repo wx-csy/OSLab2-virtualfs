8583f4e231db5e088fd1c4ee21894418e6ad9145
tracer <tracer@tracer.org>
2018-06-16 22:13:07 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

---
+++ src/test/testshell.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <test.h>
+
+struct {
+  char *name;
+  int (*fn)();
+  int result;
+} testcases = {
+  {"Multi-threading Test (1)", test_mttest, 0},
+  {"Multi-threading Test (2)", test_mttest2, 0},
+  {"procfs Functional Test", test_procfs, 0},
+  {"devfs Functional Test", test_devfs, 0},
+  {"kvfs Functional Test", test_kvfs, 0},
+  {"Concurrent R/W Test", test_concrw, 0}
+};
+
+void testshell(void *igonre) {
+  
+  test_mttest();
+  test_mttest2();
+  test_procfs(); 
+  test_devfs();
+  test_kvfs();
+  test_concrw();
+
+  _Exit(0);
+}
+
