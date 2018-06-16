fd95fb5cdf2d49b341cafd425058f1190d170243
tracer <tracer@tracer.org>
2018-06-16 18:34:30 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/test/mttest.c
+++ src/test/mttest.c
@@ -11,7 +11,7 @@ static spinlock_t io_lock;
 static sem_t sem;
 
 int is_prime(int x) {
-  for (int i = 2; i * i < x; i++) {
+  for (int i = 2; i * i <= x; i++) {
     if (x % i == 0) return 0;
   }
   return 1;
