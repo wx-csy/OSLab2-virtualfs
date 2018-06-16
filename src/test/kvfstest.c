fad4831194fc9004ece081546d57abc70a8bfde2
tracer <tracer@tracer.org>
2018-06-16 19:41:32 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/test/kvfstest.c
+++ src/test/kvfstest.c
@@ -15,7 +15,7 @@ int test_kvfstest() {
   int chk_cnt = 0;
   
   if (sh_mount("/test/", New(kvfs, "kvfs_test")) == 0) chk_cnt++;
-  if (sh_unmount("/test/") == 0) chk_cnt++;
+//  if (sh_unmount("/test/") == 0) chk_cnt++;
 
   VERDICT(chk_cnt == 2 ? 0 : 1,
       "%d of 2 checkpoints passed", chk_cnt);
