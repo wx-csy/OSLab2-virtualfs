69e25ba5b137377c1f6e22c900afabeb122d58da
tracer <tracer@tracer.org>
2018-06-16 20:08:13 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/test/kvfstest.c
+++ src/test/kvfstest.c
@@ -19,18 +19,20 @@ int test_kvfstest() {
   if (sh_access("/test/inexist", R_OK | W_OK) < 0) chk_cnt++;
   
   int fd = sh_open("/test/file0", O_RDWR);
+  if (fd >= 0) chk_cnt++;
   char buf[512];
   if (sh_read(fd, buf, 10) == 0) chk_cnt++;
   const char test_text[] = "the quick brown fox jumps over a lazy dog\n";
   if (sh_write(fd, test_text, sizeof test_text) == sizeof test_text) chk_cnt++;
-
+  if (sh_close(fd) == 0) chk_cnt++;
   if (shb_type("/test/file0") == 0) chk_cnt++;
+  
 
   shb_ls();
   
   if (sh_unmount("/test/") == 0) chk_cnt++;
   
-  VERDICT(chk_cnt == 2 ? 0 : 1,
+  VERDICT(chk_cnt == 9 ? 0 : 1,
       "%d of 2 checkpoints passed", chk_cnt);
 }
 
